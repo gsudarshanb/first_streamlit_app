88a3ae602b1b7c9f3ba8eb7be3f8688d55d153a2
Sudarshana Babu Gowrabathuni <107924739+gsudarshanb@users.noreply.github.com>
2022-06-27 00:11:14 -0700

    Update streamlite_app.py
    Modified to get all the rows, Not Just One

--- streamlite_app.py
+++ streamlite_app.py
@@ -50,3 +50,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_row)
+
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit Load List Contains:")
+streamlit.dataframe(my_data_rows)
