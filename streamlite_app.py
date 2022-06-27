c27e3f1a6ac550b8739fce8bc20c5c1cf4e0aa48
Sudarshana Babu Gowrabathuni <107924739+gsudarshanb@users.noreply.github.com>
2022-06-27 01:58:01 -0700

    Update streamlite_app.py
    Syntax error

--- streamlite_app.py
+++ streamlite_app.py
@@ -30,7 +30,7 @@ try:
   fruit_choice = streamlit.text_input('What Fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please Select a Fruit to get information.")
-  else
+  else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
